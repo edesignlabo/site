eeb119792a4f9156430c03e193bf459780aac072
edesignlabo <208324927+edesignlabo@users.noreply.github.com>
2026-05-25 12:39:39 +0900

    Add files via upload

--- emoji/parts.js
+++ emoji/parts.js
@@ -11,17 +11,17 @@ function initResponsive(){
     document.write('<link href="_/style_pc.css" type="text/css" rel="stylesheet"/>')
 }
 
-//function showUa(){
-//  var _UA = navigator.userAgent;
-//  document.write('<div style="font-size:67%;color:#000000" align="right">');
-//  if( _UA.indexOf('iPhone') >0 || _UA.indexOf('iPod') >0 || _UA.indexOf('Android') >0 )
-//    document.write('Smartphone')
-//  else if( _UA.indexOf('iPad') >0 )
-//    document.write('iPad')
-//  else
-//    document.write('PC')
-//  document.write('</div>');
-//}
+function showUa(){
+  var _UA = navigator.userAgent;
+  document.write('<!--div style="font-size:67%;color:#000000" align="right"-->');
+  if( _UA.indexOf('iPhone') >0 || _UA.indexOf('iPod') >0 || _UA.indexOf('Android') >0 )
+    document.write('Smartphone')
+  else if( _UA.indexOf('iPad') >0 )
+    document.write('iPad')
+  else
+    document.write('PC')
+  document.write('<!--/div-->');
+}
 
 //function body(){
 //  document.write('<body class="bodySettings">')
